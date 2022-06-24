ebfd4ea9da1b40e3f775dcfb002429199885b62c
maudetes <maudet.estelle@gmail.com>
2022-06-24 10:31:43 +0200

    Use dynamic version

--- udata_analysis_service/__init__.py
+++ udata_analysis_service/__init__.py
@@ -3,4 +3,9 @@ udata analysis service
 """
 
 
-__version__ = "0.0.0"
+import os
+
+tag = os.environ.get('CIRCLE_TAG')
+build_num = os.environ.get('CIRCLE_BUILD_NUM')
+
+__version__ = '0.0.1.dev' + (str(build_num) if not tag and build_num else '')
