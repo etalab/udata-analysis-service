9f9e4753a1d8e240c79688ab1d4253e55d135915
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-07-12 11:35:53 +0200

    fix consumer

--- udata_analysis_service/consumer.py
+++ udata_analysis_service/consumer.py
@@ -5,13 +5,15 @@ from udata_analysis_service.background_tasks import manage_resource
 
 
 def process_message(resource_id: str, message: dict, topic: str) -> None:
-    logging.info(f"New message detected, topic {topic}, checking resource {resource_id}")
+    logging.info(
+        f"New message detected, topic {topic}, checking resource {resource_id}"
+    )
 
     dataset_id = message["meta"]["dataset_id"]
     manage_resource.delay(
         dataset_id,
         resource_id,
-        message["value"],
+        message["value"]["location"],
         minio_user=os.environ["MINIO_USER"],
         minio_pwd=os.environ["MINIO_PWD"],
     )
