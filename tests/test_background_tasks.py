6e51716595f91c52bc1b4a3b174c052124f56531
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-07-13 15:33:17 +0200

    Share csv details (#7)
    * retrieve CSV encoding and delimiter from Kafka
    
    * fix consumer
    
    * update csv-detective version and remove requirements.txt
    
    * add csv location and tableschema location in Kafka message
    
    * update test
    
    * fix syntax error
    
    * update kafka message structure

--- tests/test_background_tasks.py
+++ tests/test_background_tasks.py
@@ -9,38 +9,59 @@ pytestmark = pytest.mark.asyncio
 
 
 async def test_manage_resource_send_produce_message(mocker):
-    mocker.patch.dict(os.environ, {
-        'CSV_DETECTIVE_REPORT_BUCKET': 'detective-bucket',
-        'CSV_DETECTIVE_REPORT_FOLDER': 'report',
-        'TABLESCHEMA_BUCKET': '',
-        'TABLESCHEMA_FOLDER': '',
-        'KAFKA_HOST': 'localhost',
-        'KAFKA_PORT': '9092',
-        'UDATA_INSTANCE_NAME': 'udata'
-    })
-    mocker.patch('boto3.client')
-    routine = mocker.patch('udata_analysis_service.background_tasks.routine_minio')
-    produce = mocker.patch('udata_analysis_service.background_tasks.produce')
+    mocker.patch.dict(
+        os.environ,
+        {
+            "CSV_DETECTIVE_REPORT_BUCKET": "detective-bucket",
+            "CSV_DETECTIVE_REPORT_FOLDER": "report",
+            "TABLESCHEMA_BUCKET": "tableschema-bucket",
+            "TABLESCHEMA_FOLDER": "tableschema-folder",
+            "KAFKA_HOST": "localhost",
+            "KAFKA_PORT": "9092",
+            "UDATA_INSTANCE_NAME": "udata",
+        },
+    )
+    mocker.patch("boto3.client")
+    routine = mocker.patch(
+        "udata_analysis_service.background_tasks.routine_minio"
+    )
+    produce = mocker.patch("udata_analysis_service.background_tasks.produce")
 
     manage_resource(
-        dataset_id = 'dataset_id',
-        resource_id = 'resource_id',
-        resource_location = {'netloc': 'netloc', 'bucket': 'bucket', 'key': 'key'},
-        minio_user = 'minio_user',
-        minio_pwd = 'minio_pwd',
+        dataset_id="dataset_id",
+        resource_id="resource_id",
+        resource_details={
+            "location": {"netloc": "netloc", "bucket": "bucket", "key": "key"},
+            "encoding": "UTF-8",
+            "delimiter": ",",
+        },
+        minio_user="minio_user",
+        minio_pwd="minio_pwd",
     )
 
     produce.assert_called_with(
-        'localhost:9092',
-        'udata.resource.analysed',
-        service='csvdetective',
-        key_id='resource_id',
+        "localhost:9092",
+        "udata.resource.analysed",
+        service="csvdetective",
+        key_id="resource_id",
         document={
-            'location': {
-                'url': 'netloc',
-                'bucket': 'detective-bucket',
-                'key': 'report/dataset_id/resource_id.json'
-            }
+            "data_location": {
+                "url": "netloc",
+                "bucket": "bucket",
+                "key": "key",
+            },
+            "report_location": {
+                "url": "netloc",
+                "bucket": "detective-bucket",
+                "key": "report/dataset_id/resource_id.json",
+            },
+            "tableschema_location": {
+                "url": "netloc",
+                "bucket": "tableschema-bucket",
+                "key": "tableschema-folder/dataset_id/resource_id",
+            },
+            "encoding": "UTF-8",
+            "delimiter": ",",
         },
-        meta={'dataset_id': 'dataset_id', 'message_type': 'resource.analysed'},
+        meta={"dataset_id": "dataset_id", "message_type": "resource.analysed"},
     )
